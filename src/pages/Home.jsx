caec3f6df58af0bc930d3157dabc72f9ff06989e
DorukEmre <doruk.emre@outlook.com>
2022-10-23 18:26:16 +0100

    Reorganised root app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import { catMainLogo } from '../assets/images'
 
-const Main = () => {
+const Home = () => {
   return (
     <main id="home-page">
       <section className="hero-container">
         <div className="hero-title-container">
-          <h1>Marcel <span>the outdoor cat</span></h1>
+          <h1>
+            Marcel <span>the outdoor cat</span>
+          </h1>
         </div>
         <div className="hero-image-container">
-          <img src={ catMainLogo } alt="cat logo" />
+          <img src={catMainLogo} alt="cat logo" />
         </div>
       </section>
     </main>
   )
 }
 
-export default Main
+export default Home
